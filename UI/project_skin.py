dbce559849f31bfaa67a0d158ff09b24ddb4600d
LotusPetal1000 <33.gsingh@gmail.com>
2025-12-27 16:48:13 +0000

    Add untracked files: UI/Logo.ico, UI/project_skin.py, storage.py on 2025-12-27T16:48:13.2241837+00:00

---
+++ UI/project_skin.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import json
+
+root = tk.Tk()
+root.title("Basic App")
+root.geometry("600x400")
+root.configure(bg= "#000000")
+root.iconbitmap("Logo.ico")
+
+root.mainloop()
---
+++ storage.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+def save_dict(file_name: str, dict_name: dict):
+    with open(file_name, "w", encoding = "utf-8") as f:
+        json.dump(dict_name, f, ensure_ascii= False, indent= 4)
+
+def load_dict(file_name: str):
+    if not os.path.exists(file_name):
+        return {}
+    with open(file_name, "r", encoding = "utf-8") as f:
+        return json.load(f)
